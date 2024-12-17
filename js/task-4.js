edf1ab474894b9aa05795495d97febfc8173b9cc
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2024-12-17 13:09:19 +0300

    task -4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('.login-form');
+console.dir(formEl);
+formEl.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const email = formEl.elements.email.value;
+    const password = formEl.elements.password.value;
+
+    const formData = {
+       
+        [email]: formEl.elements.email.value,
+        [password]: formEl.elements.password.value,
+    };
+
+    if (email === '' || password === '') {
+    return alert('All form fields must be filled in');
+    
+}
+
+    console.log(`email: ${email}, password: ${password}`);
+    
+    formEl.reset();
+});
 
