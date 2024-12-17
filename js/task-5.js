a9f0221180a0337787e20826ed3adcf8dd2913cf
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2024-12-17 14:16:55 +0300

    task -5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bntEl = document.querySelector('.change-color');
+console.log(bntEl);
+const spanColor = document.querySelector('.color');
+console.dir( spanColor);
+
+bntEl.addEventListener('click', event => {
+  spanColor.style.backgroundcolor = getRandomHexColor();
+});
+
